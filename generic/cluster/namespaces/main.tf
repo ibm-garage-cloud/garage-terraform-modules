a19e244ae25333b8a50ba5f7a332b0fb8380ea99
Sean Sundberg <seansund@us.ibm.com>
2020-01-27 15:38:39 +0100

    Updates terraform modules to support OCP 4.3 deployed with terraform 0.12

--- generic/cluster/namespaces/main.tf
+++ generic/cluster/namespaces/main.tf
@@ -2,92 +2,97 @@ provider "null" {
 }
 
 locals {
-  tools_namespace = ["${var.tools_namespace}"]
-  namespaces      = "${concat(local.tools_namespace, var.other_namespaces)}"
+  tools_namespace = [var.tools_namespace]
+  namespaces      = concat(local.tools_namespace, var.other_namespaces)
 }
 
 resource "null_resource" "delete_namespaces" {
-  count      = "${length(local.namespaces)}"
+  count      = length(local.namespaces)
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/deleteNamespace.sh ${local.namespaces[count.index]}"
 
     environment = {
-      KUBECONFIG_IKS = "${var.cluster_config_file_path}"
+      KUBECONFIG_IKS = var.cluster_config_file_path
     }
   }
 }
 
 resource "null_resource" "create_namespaces" {
-  depends_on = ["null_resource.delete_namespaces"]
-  count      = "${length(local.namespaces)}"
+  depends_on = [null_resource.delete_namespaces]
+  count      = length(local.namespaces)
+
+  triggers = {
+    namespace      = local.namespaces[count.index]
+    kubeconfig_iks = var.cluster_config_file_path
+  }
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/createNamespace.sh ${local.namespaces[count.index]}"
+    command = "${path.module}/scripts/createNamespace.sh ${self.triggers.namespace}"
 
     environment = {
-      KUBECONFIG_IKS = "${var.cluster_config_file_path}"
+      KUBECONFIG_IKS = self.triggers.kubeconfig_iks
     }
   }
 
   provisioner "local-exec" {
-    when    = "destroy"
-    command = "${path.module}/scripts/deleteNamespace.sh ${local.namespaces[count.index]}"
+    when    = destroy
+    command = "${path.module}/scripts/deleteNamespace.sh ${self.triggers.namespace}"
 
     environment = {
-      KUBECONFIG_IKS = "${var.cluster_config_file_path}"
+      KUBECONFIG_IKS = self.triggers.kubeconfig_iks
     }
   }
 }
 
 resource "null_resource" "copy_tls_secrets" {
-  depends_on = ["null_resource.create_namespaces"]
-  count      = "${length(local.namespaces)}"
+  depends_on = [null_resource.create_namespaces]
+  count      = length(local.namespaces)
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/copy-secret-to-namespace.sh \"${var.tls_secret_name}\" ${local.namespaces[count.index]}"
 
     environment = {
-      KUBECONFIG_IKS = "${var.cluster_config_file_path}"
+      KUBECONFIG_IKS = var.cluster_config_file_path
     }
   }
 }
 
 resource "null_resource" "copy_apikey_secret" {
-  depends_on = ["null_resource.create_namespaces"]
-  count      = "${length(local.namespaces)}"
+  depends_on = [null_resource.create_namespaces]
+  count      = length(local.namespaces)
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/copy-secret-to-namespace.sh ibmcloud-apikey ${local.namespaces[count.index]}"
 
     environment = {
-      KUBECONFIG_IKS = "${var.cluster_config_file_path}"
+      KUBECONFIG_IKS = var.cluster_config_file_path
     }
   }
 }
 
 resource "null_resource" "create_pull_secrets" {
-  depends_on = ["null_resource.create_namespaces"]
-  count      = "${length(local.namespaces)}"
+  depends_on = [null_resource.create_namespaces]
+  count      = length(local.namespaces)
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/setup-namespace-pull-secrets.sh ${local.namespaces[count.index]}"
 
     environment = {
-      KUBECONFIG_IKS = "${var.cluster_config_file_path}"
+      KUBECONFIG_IKS = var.cluster_config_file_path
     }
   }
 }
 
 resource "null_resource" "copy_cloud_configmap" {
-  depends_on = ["null_resource.create_namespaces"]
-  count      = "${length(local.namespaces)}"
+  depends_on = [null_resource.create_namespaces]
+  count      = length(local.namespaces)
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/copy-configmap-to-namespace.sh ibmcloud-config ${local.namespaces[count.index]}"
 
     environment = {
-      KUBECONFIG_IKS = "${var.cluster_config_file_path}"
+      KUBECONFIG_IKS = var.cluster_config_file_path
     }
   }
 }
--- generic/cluster/namespaces/outputs.tf
+++ generic/cluster/namespaces/outputs.tf
@@ -1,11 +1,11 @@
 output "tools_namespace_name" {
-  value       = "${var.tools_namespace}"
+  value       = var.tools_namespace
   description = "Tools namespace name"
-  depends_on  = ["null_resource.create_namespaces"]
+  depends_on  = [null_resource.create_namespaces]
 }
 
 output "other_namespaces" {
-  value       = "${var.other_namespaces}"
+  value       = var.other_namespaces
   description = "Other namespaces"
   depends_on  = ["null_resource.create_namespaces"]
 }
