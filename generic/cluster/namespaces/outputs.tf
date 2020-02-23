555596f29fafb42eabbed528954b847f169c71f2
Sean Sundberg <seansund@us.ibm.com>
2020-02-23 09:02:48 -0600

    Trying to fix dependency issue in namespaces

--- generic/cluster/namespaces/outputs.tf
+++ generic/cluster/namespaces/outputs.tf
@@ -1,11 +1,11 @@
 output "tools_namespace_name" {
   value       = var.tools_namespace
   description = "Namespace where development tools will be deployed"
-  depends_on  = [null_resource.create_namespaces]
+  depends_on  = [null_resource.create_namespaces[0]]
 }
 
 output "release_namespaces" {
   value       = var.release_namespaces
   description = "Namespaces where applications will be deployed"
-  depends_on  = [null_resource.create_namespaces]
+  depends_on  = [null_resource.create_namespaces.*]
 }
