ce7287835ff3bd8fed9401097f5726b03f5081cf
Sean Sundberg <seansund@us.ibm.com>
2020-02-24 13:17:14 -0600

    Namespace module: Streamline logic/terraform alignment
    - Updates kubernetes provider config
    - Updates namespaces module to create release namespaces with kubernetes provider
    - Adds dependency with delete_tools_namespace in namespace module
    - Updates namespace module logic

--- generic/cluster/namespaces/main.tf
+++ generic/cluster/namespaces/main.tf
@@ -1,5 +1,7 @@
-provider "null" {
+provider "kubernetes" {
+  config_path = var.cluster_config_file_path
 }
+provider "null" {}
 
 locals {
   tools_namespace = [var.tools_namespace]
@@ -28,61 +30,79 @@ resource "null_resource" "delete_release_namespaces" {
   }
 }
 
-resource "null_resource" "create_tools_namespace" {
+resource "kubernetes_namespace" "tools" {
   depends_on = [null_resource.delete_tools_namespace]
 
-  triggers = {
-    namespace      = var.tools_namespace
-    kubeconfig_iks = var.cluster_config_file_path
-  }
-
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/createNamespace.sh ${self.triggers.namespace}"
-
-    environment = {
-      KUBECONFIG_IKS = self.triggers.kubeconfig_iks
-    }
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "${path.module}/scripts/deleteNamespace.sh ${self.triggers.namespace}"
-
-    environment = {
-      KUBECONFIG_IKS = self.triggers.kubeconfig_iks
-    }
+  metadata {
+    name = var.tools_namespace
   }
 }
+//
+//resource "null_resource" "create_tools_namespace" {
+//  depends_on = [null_resource.delete_tools_namespace]
+//
+//  triggers = {
+//    namespace      = var.tools_namespace
+//    kubeconfig_iks = var.cluster_config_file_path
+//  }
+//
+//  provisioner "local-exec" {
+//    command = "${path.module}/scripts/createNamespace.sh ${self.triggers.namespace}"
+//
+//    environment = {
+//      KUBECONFIG_IKS = self.triggers.kubeconfig_iks
+//    }
+//  }
+//
+//  provisioner "local-exec" {
+//    when    = destroy
+//    command = "${path.module}/scripts/deleteNamespace.sh ${self.triggers.namespace}"
+//
+//    environment = {
+//      KUBECONFIG_IKS = self.triggers.kubeconfig_iks
+//    }
+//  }
+//}
 
-resource "null_resource" "create_release_namespaces" {
+
+resource "kubernetes_namespace" "releases" {
   depends_on = [null_resource.delete_release_namespaces]
   count      = length(var.release_namespaces)
 
-  triggers = {
-    namespace      = var.release_namespaces[count.index]
-    kubeconfig_iks = var.cluster_config_file_path
-  }
-
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/createNamespace.sh ${self.triggers.namespace}"
-
-    environment = {
-      KUBECONFIG_IKS = self.triggers.kubeconfig_iks
-    }
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "${path.module}/scripts/deleteNamespace.sh ${self.triggers.namespace}"
-
-    environment = {
-      KUBECONFIG_IKS = self.triggers.kubeconfig_iks
-    }
+  metadata {
+    name = var.release_namespaces[count.index]
   }
 }
+//
+//resource "null_resource" "create_release_namespaces" {
+//  depends_on = [null_resource.delete_release_namespaces]
+//  count      = length(var.release_namespaces)
+//
+//  triggers = {
+//    namespace      = var.release_namespaces[count.index]
+//    kubeconfig_iks = var.cluster_config_file_path
+//  }
+//
+//  provisioner "local-exec" {
+//    command = "${path.module}/scripts/createNamespace.sh ${self.triggers.namespace}"
+//
+//    environment = {
+//      KUBECONFIG_IKS = self.triggers.kubeconfig_iks
+//    }
+//  }
+//
+//  provisioner "local-exec" {
+//    when    = destroy
+//    command = "${path.module}/scripts/deleteNamespace.sh ${self.triggers.namespace}"
+//
+//    environment = {
+//      KUBECONFIG_IKS = self.triggers.kubeconfig_iks
+//    }
+//  }
+//}
 
 resource "null_resource" "copy_tls_secrets" {
-  depends_on = [null_resource.create_tools_namespace, null_resource.create_release_namespaces]
+  depends_on = [kubernetes_namespace.tools, kubernetes_namespace.releases]
   count      = length(local.namespaces)
 
   provisioner "local-exec" {
@@ -95,7 +115,7 @@ resource "null_resource" "copy_tls_secrets" {
 }
 
 resource "null_resource" "copy_apikey_secret" {
-  depends_on = [null_resource.create_tools_namespace, null_resource.create_release_namespaces]
+  depends_on = [kubernetes_namespace.tools, kubernetes_namespace.releases]
   count      = length(local.namespaces)
 
   provisioner "local-exec" {
@@ -108,7 +128,7 @@ resource "null_resource" "copy_apikey_secret" {
 }
 
 resource "null_resource" "create_pull_secrets" {
-  depends_on = [null_resource.create_tools_namespace, null_resource.create_release_namespaces]
+  depends_on = [kubernetes_namespace.tools, kubernetes_namespace.releases]
   count      = length(local.namespaces)
 
   provisioner "local-exec" {
@@ -121,7 +141,7 @@ resource "null_resource" "create_pull_secrets" {
 }
 
 resource "null_resource" "copy_cloud_configmap" {
-  depends_on = [null_resource.create_tools_namespace, null_resource.create_release_namespaces]
+  depends_on = [kubernetes_namespace.tools, kubernetes_namespace.releases]
   count      = length(local.namespaces)
 
   provisioner "local-exec" {
--- generic/cluster/namespaces/outputs.tf
+++ generic/cluster/namespaces/outputs.tf
@@ -1,11 +1,11 @@
 output "tools_namespace_name" {
   value       = var.tools_namespace
   description = "Namespace where development tools will be deployed"
-  depends_on  = [null_resource.create_tools_namespace]
+  depends_on  = [kubernetes_namespace.tools]
 }
 
 output "release_namespaces" {
   value       = var.release_namespaces
   description = "Namespaces where applications will be deployed"
-  depends_on  = [null_resource.create_release_namespaces]
+  depends_on  = [kubernetes_namespace.releases]
 }
