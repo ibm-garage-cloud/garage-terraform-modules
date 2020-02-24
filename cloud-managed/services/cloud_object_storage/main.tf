837cc8691625977bb4e2b38764ffc29710329d00
Sean Sundberg <seansund@us.ibm.com>
2020-02-24 12:55:50 -0600

    Updates appid, cos, and cloudant modules to bind to service by id instead of name to prevent error when services with duplicate names exist

--- cloud-managed/services/cloud_object_storage/main.tf
+++ cloud-managed/services/cloud_object_storage/main.tf
@@ -43,7 +43,7 @@ resource "ibm_container_bind_service" "cos_binding" {
   count      = var.namespace_count
 
   cluster_name_id             = var.cluster_id
-  service_instance_name       = ibm_resource_instance.cos_instance.name
+  service_instance_id         = ibm_resource_instance.cos_instance.id
   namespace_id                = var.namespaces[count.index]
   resource_group_id           = data.ibm_resource_group.tools_resource_group.id
   key                         = ibm_resource_key.cos_credentials.name
