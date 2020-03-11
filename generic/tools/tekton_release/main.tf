5d5bc27faad4b416d8cfa5eda8ba4df6d2ea62f8
Carlos Santana <csantana23@gmail.com>
2020-03-11 16:47:54 -0400

    add destroy scripts for tekton modules

--- generic/tools/tekton_release/main.tf
+++ generic/tools/tekton_release/main.tf
@@ -7,22 +7,50 @@ locals {
 }
 
 resource "null_resource" "tekton" {
+
+  triggers = {
+    kubeconfig = var.cluster_config_file_path
+  }
+  
   provisioner "local-exec" {
     command = "${path.module}/scripts/deploy-tekton.sh"
 
     environment = {
-      KUBECONFIG = var.cluster_config_file_path
+      KUBECONFIG = self.triggers.kubeconfig
+    }
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "${path.module}/scripts/destroy-tekton.sh"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
     }
   }
 }
 
 resource "null_resource" "tekton_dashboard" {
-    depends_on = [null_resource.tekton]
+  depends_on = [null_resource.tekton]
+
+  triggers = {
+    kubeconfig = var.cluster_config_file_path
+  }
+
   provisioner "local-exec" {
     command = "${path.module}/scripts/deploy-tekton-dashboard.sh ${local.ingress_host} ${local.namespace}"
 
     environment = {
-      KUBECONFIG = var.cluster_config_file_path
+      KUBECONFIG = self.triggers.kubeconfig
+    }
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "${path.module}/scripts/destroy-tekton-dashboard.sh ${local.namespace}"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
     }
   }
 }
@@ -30,11 +58,25 @@ resource "null_resource" "tekton_dashboard" {
 resource "null_resource" "copy_cloud_configmap" {
   depends_on = [null_resource.tekton_dashboard]
 
+  triggers = {
+    kubeconfig         = var.cluster_config_file_path
+    tools_namespace    = var.tools_namespace
+  }
+
   provisioner "local-exec" {
-    command = "${path.module}/scripts/copy-configmap-to-namespace.sh tekton-config ${var.tools_namespace} ${local.namespace}"
+    command = "${path.module}/scripts/copy-configmap-to-namespace.sh tekton-config ${self.triggers.tools_namespace} ${local.namespace}"
 
     environment = {
-      KUBECONFIG = var.cluster_config_file_path
+      KUBECONFIG = self.triggers.kubeconfig
+    }
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "${path.module}/scripts/destroy-configmap.sh tekton-config ${self.triggers.tools_namespace}"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
     }
   }
 }
