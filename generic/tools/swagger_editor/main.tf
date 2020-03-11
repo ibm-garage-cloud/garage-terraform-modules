5c828bc483519777abb36c97bd379fd239d1ba7c
Sean Sundberg <seansund@us.ibm.com>
2020-03-10 22:17:18 -0500

    Adds kubeconfig value to triggers for swagger module

--- generic/tools/swagger_editor/main.tf
+++ generic/tools/swagger_editor/main.tf
@@ -5,14 +5,15 @@ locals {
 
 resource "null_resource" "swaggereditor_release" {
   triggers = {
-    namespace = var.releases_namespace
+    namespace  = var.releases_namespace
+    kubeconfig = var.cluster_config_file
   }
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/deploy-swaggereditor.sh ${self.triggers.namespace} ${var.cluster_type} apieditor ${var.cluster_ingress_hostname} ${var.image_tag}"
 
     environment = {
-      KUBECONFIG_IKS  = var.cluster_config_file
+      KUBECONFIG_IKS  = self.triggers.kubeconfig
       TLS_SECRET_NAME = var.tls_secret_name
       TMP_DIR         = local.tmp_dir
     }
@@ -23,8 +24,7 @@ resource "null_resource" "swaggereditor_release" {
     command = "${path.module}/scripts/destroy-swaggereditor.sh ${self.triggers.namespace}"
 
     environment = {
-      KUBECONFIG_IKS = var.cluster_config_file
-      KUBECONFIG_IKS = var.cluster_config_file
+      KUBECONFIG_IKS = self.triggers.kubeconfig
     }
   }
 }
