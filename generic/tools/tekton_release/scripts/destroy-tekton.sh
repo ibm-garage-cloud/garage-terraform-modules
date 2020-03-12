feb1dde7f33ca283fed6763d857fe78bca7d0c38
Carlos Santana <csantana23@gmail.com>
2020-03-11 23:18:57 -0400

    delete the operator cr

--- generic/tools/tekton_release/scripts/destroy-tekton.sh
+++ generic/tools/tekton_release/scripts/destroy-tekton.sh
@@ -8,3 +8,5 @@ YAML_FILE=${MODULE_DIR}/tekton.yaml
 echo "*** deleting tekton openshift-pipelines-operator"
 kubectl delete -f ${YAML_FILE} || true
 
+# TODO: do this delete in a more generic way
+oc delete ClusterServiceVersion openshift-pipelines-operator.v0.10.7 -n openshift-operators || true
