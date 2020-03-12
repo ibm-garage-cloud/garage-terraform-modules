3662c386e3b5c1dde2a027da74ae199df0212313
Carlos Santana <csantana23@gmail.com>
2020-03-11 23:32:03 -0400

    revert cleaning too much

--- generic/tools/tekton_release/scripts/destroy-tekton.sh
+++ generic/tools/tekton_release/scripts/destroy-tekton.sh
@@ -7,6 +7,3 @@ YAML_FILE=${MODULE_DIR}/tekton.yaml
 
 echo "*** deleting tekton openshift-pipelines-operator"
 kubectl delete -f ${YAML_FILE} || true
-
-# TODO: do this delete in a more generic way
-oc delete ClusterServiceVersion openshift-pipelines-operator.v0.10.7 -n openshift-operators || true
