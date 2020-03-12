242add13abb29d8d18464a1c23cd884f8ddc9ebc
Carlos Santana <csantana23@gmail.com>
2020-03-12 18:04:10 -0400

    add comment on destroy sonarqube

--- self-managed/software/postgres/scripts/destroy-postgres.sh
+++ self-managed/software/postgres/scripts/destroy-postgres.sh
@@ -7,6 +7,7 @@ if [[ -n "${KUBECONFIG_IKS}" ]]; then
     export KUBECONFIG="${KUBECONFIG_IKS}"
 fi
 
+echo "Destroying sonarqube resources"
 kubectl delete all -l "app=${APP_NAME}" -n "${NAMESPACE}" || true
 kubectl delete secret -l "app=${APP_NAME}" -n "${NAMESPACE}" || true
 kubectl delete pvc -l "app=${APP_NAME}" -n "${NAMESPACE}" || true
