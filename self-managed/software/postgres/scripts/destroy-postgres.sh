f3c8c090c154ea9d940d5b7192a6153b57feaab7
Carlos Santana <csantana@us.ibm.com>
2020-03-13 22:29:06 -0400

    fix delete postgres (#32)

--- self-managed/software/postgres/scripts/destroy-postgres.sh
+++ self-managed/software/postgres/scripts/destroy-postgres.sh
@@ -7,7 +7,5 @@ if [[ -n "${KUBECONFIG_IKS}" ]]; then
     export KUBECONFIG="${KUBECONFIG_IKS}"
 fi
 
-echo "Destroying sonarqube resources"
-kubectl delete all -l "app=${APP_NAME}" -n "${NAMESPACE}" || true
-kubectl delete secret -l "app=${APP_NAME}" -n "${NAMESPACE}" || true
-kubectl delete pvc -l "app=${APP_NAME}" -n "${NAMESPACE}" || true
+echo "Destroying postgres db"
+kubectl delete deploymentconfig,service,secret,pvc -l "app=${APP_NAME}" -n "${NAMESPACE}"
