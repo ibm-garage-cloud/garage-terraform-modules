8378eb9aefaed57ff003a4446f3c2089724fb04d
Carlos Santana <csantana23@gmail.com>
2020-03-13 21:43:08 -0400

    parametized git for tekton tasks

--- generic/tools/tekton_resources/main.tf
+++ generic/tools/tekton_resources/main.tf
@@ -13,7 +13,7 @@ resource "null_resource" "tekton_resources" {
   }
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/deploy-tekton-resources.sh ${self.triggers.tools_namespace} ${var.pre_tekton} ${var.tekton_tools_resources_revision}"
+    command = "${path.module}/scripts/deploy-tekton-resources.sh ${self.triggers.tools_namespace} ${var.pre_tekton} ${var.revision} ${var.git_url}"
 
     environment = {
       KUBECONFIG       = self.triggers.kubeconfig
